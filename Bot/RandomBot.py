ec0bd3a1d933e12f98c15942b5302feb5a21db07
Titusz Ban <tituszban@gmail.com>
2018-11-07 20:39:34 +0000

    Add Random Bot

---
+++ Bot/RandomBot.py
@@ -0,0 +1,27 @@
+from Bot import Bot
+import numpy as np
+import random
+
+
+class RandomBot(Bot):
+    def __init__(self):
+        self.start_health = 0
+        self.lanes = 0
+
+    def on_start(self, config):
+        self.start_health = config['START_HEALTH']
+        self.lanes = config['LANES']
+
+    def on_turn(self, gs):
+        attack = np.zeros(self.lanes, np.int32)
+        defense = np.zeros(self.lanes, np.int32)
+
+        for i in range(int(np.floor(gs.my_info.cores))):
+            defense[random.randrange(self.lanes)] += 1
+        for i in range(int(np.floor(gs.my_info.bits))):
+            attack[random.randrange(self.lanes)] += 1
+        print(attack, defense)
+        return attack, defense
+
+    def on_game_over(self, is_winner):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from Game.GameCore import GameCore
+
+from Bot.RandomBot import RandomBot
+
 def main():
-    pass
+    bot1 = RandomBot()
+    bot2 = RandomBot()
+
+    game = GameCore(bot1, bot2)
+
+    game.game_loop()
+
+
 
 
 if __name__ == "__main__":
